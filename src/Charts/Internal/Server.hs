129a28bd2dadbbe357e4f087edf78a4201e3f146
Chris Penner <christopher.penner@gmail.com>
2020-10-24 18:41:44 -0600

    Package data files

--- src/Charts/Internal/Server.hs
+++ src/Charts/Internal/Server.hs
@@ -7,13 +7,16 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.HTTP.Types.Status
 import Data.Aeson
+import Paths_charter
 
-chartApp :: Chart a -> Application
-chartApp chart req handler 
+chartApp :: Chart a -> FilePath -> Application
+chartApp chart indexFile req handler 
       | pathInfo req == ["data"] = do
           handler (responseLBS ok200 [("Content-Type", "application/json")] (encode chart))
       | otherwise = do
-          handler (responseFile ok200 mempty "./templates/index.html" Nothing)
+          handler (responseFile ok200 mempty indexFile Nothing)
 
 serveChart :: Port -> Chart a -> IO ()
-serveChart port chart = run port (chartApp chart)
+serveChart port chart = do
+    indexHtml <- getDataFileName "templates/index.html"
+    run port (chartApp chart indexHtml)
